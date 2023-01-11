7c20dd7fd2617141f28e3b0e029adf51d7cb8d3c
muratvuranok <muratvuranok@hotmail.com>
2023-01-11 20:47:24 +0300

    mvc

---
+++ MVC/FirstMvcApp/FirstMvcApp/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace FirstMvcApp.Controllers;
+public class CategoriesController : Controller
+{
+    public IActionResult Index() { return View(); }
+    public IActionResult Create() { return View(); }
+    public IActionResult Edit() { return View(); }
+    public IActionResult Delete() { return View(); }
+    public IActionResult Details() { return View(); }
+}
---
+++ MVC/FirstMvcApp/FirstMvcApp/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace FirstMvcApp.Models;
+
+public class Category
+{
+    public string CategoryName { get; set; }
+}
